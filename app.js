9fef253a9470b4baf1b1b5cff6ce54a130a9c1f7
julesferguson456 <julesferguson456@gmail.com>
2025-02-28 17:10:27 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,88 @@
+// const movies = ["Barbie", "Interstellar", "Get Out"]
+
+// // Grab the first element in the movie array
+
+// "Barbie"
+// const firstEl = movies[1]
+// console.log(firstElnode)
+
+// movies.push("Aliens")
+// console.log(movies)
+
+// // Assign an empty array to a variable named books.
+// // Add ‘The Shining’ and ‘Pride and Prejudice’ or 2 other books of your choice to the books array.
+// // console.log() the second element of books.
+// // Update the second element by assigning to it ‘Dune’ or another book of your choice.
+// // Iterate over the entire books array and console.log() each book string.
+
+// // Step 1: Assign an empty array to a variable named books.
+
+// const books = []
+
+// // Step 2: Add ‘The Shining’ and ‘Pride and Prejudice’ or 2 other books of your choice to the books array.
+
+// push.books('The Shining', 'Pride and Prejudice');
+
+// // Step 3: // console.log() the second element of books.
+
+// console.log('Pride and Prejudice')
+
+// // Step 4: // Update the second element by assigning to it ‘Dune’ or another book of your choice.
+
+// movies[1] = "Dune"
+
+// Step 5: // Iterate over the entire books array and console.log() each book string.
+
+// movies.forEach(())
+
+
+// // JS Arrays
+// const movies = ["Barbie", "Interstellar", "Get Out"]
+
+// // Grab the first element in the movie array
+// const firstEl = movies[0]
+// console.log(firstEl)
+
+// // Length
+// console.log(movies.length)
+
+// // Get the last item in an array
+// const lastEl = movies[movies.length - 1]
+
+// // Updating an array
+// movies[1] = "Arrival"
+// console.log(movies)
+
+// // Adding and removing from the end of the array
+// movies.push("Inherent Vice", "Top Gun")
+// console.log(movies)
+
+// movies.pop()
+// console.log(movies)
+
+// // Looping
+// for(let i = 0; i < movies.length; i++) {
+//   console.log(`Movie ${movies[i]} is in index ${i}`)
+// }
+
+// // Looping using the forEach iterator method
+// movies.forEach((movie, index) => {
+//   console.log(`Movie ${movie} is in index ${index}`)
+// })
+
+// // Join
+// const joinMovies = movies.join(" ^ ")
+// console.log(typeof joinMovies)
+
+const music = {
+    currentTrack: 'Just Ken',
+    currentPlaylist: ['Just Ken', 'Hey Blondie', 'What Was I Made For', 'Dance The Night'];
+    volume: 70,
+    myPlaylist: 'Linkin Park', 'Bob Marley', 'Coldplay',
+}
+
+console.log(myPlaylist)
+
+delete myPlaylist
+
+console.log(music)
